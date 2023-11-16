3a86d928682adacd508b963848fb53683e1719a6
martinLisi82ORT <martin.lisi1212@gmail.com>
2023-11-16 20:58:57 -0300

    .env

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-const PORT = 8080
+import dotenv from 'dotenv'
 
-const MODO_PERSISTENCIA = 'FILE'  // 'MEM', 'FILE', 'MONGODB'
-const STRCNX = 'mongodb://127.0.0.1'
-const BASE = 'mibase'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+const MODO_PERSISTENCIA = process.env.MODO_PERSISTENCIA || 'FILE'  // 'MEM', 'FILE', 'MONGODB'
+const STRCNX = process.env.STRCNX || 'mongodb://127.0.0.1'
+const BASE = process.env.BASE || 'mibase'
 
 export default {
     PORT,
